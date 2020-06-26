8302fdcc1d1ed231fdf45f64745d55a57e886695
Joseph Emmatty Johny <jemmatty@rmplc.net>
2020-06-26 12:35:53 +0530

    Added play and format button

--- src/components/editor/codeview.tsx
+++ src/components/editor/codeview.tsx
@@ -17,9 +17,46 @@ const CodeView: React.FunctionComponent<Props> = (props) => {
     lineNumbers: true,
   };
 
+  const getHeaderControls = () => {
+    let playButton = (
+      <button type="button" className="codeview-controls" title="Execute">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24"
+          viewBox="0 0 24 24"
+          width="24"
+        >
+          <path d="M0 0h24v24H0z" fill="none" />
+          <path d="M8 5v14l11-7z" fill="#51de51" className="playbtn" />
+        </svg>
+        Play
+      </button>
+    );
+
+    let formatButton = (
+      <button type="button" className="codeview-controls" title="Format code">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24"
+          viewBox="0 0 24 24"
+          width="15"
+        >
+          <path d="M0 0h24v24H0z" fill="none" />
+          <path
+            d="M15 15H3v2h12v-2zm0-8H3v2h12V7zM3 13h18v-2H3v2zm0 8h18v-2H3v2zM3 3v2h18V3H3z"
+            fill="white"
+          />
+        </svg>
+        &nbsp;Fromat
+      </button>
+    );
+
+    return [playButton, formatButton];
+  };
+
   return (
     <React.Fragment>
-      <ViewHeader title="JavaScript" />
+      <ViewHeader title="JavaScript" controls={getHeaderControls()} />
       <CodeMirror
         className="codemirror-wrapper"
         options={options}
--- src/components/editor/viewheader.tsx
+++ src/components/editor/viewheader.tsx
@@ -2,10 +2,17 @@ import React from "react";
 
 interface Props {
   title: string;
+  classname?: string;
+  controls?: JSX.Element[];
 }
 
 const ViewHeader: React.FunctionComponent<Props> = (props) => {
-  return <div className="viewHeader">{props.title}</div>;
+  return (
+    <div className="viewHeader">
+      {props.title}
+      {props.controls}
+    </div>
+  );
 };
 
 export default ViewHeader;
