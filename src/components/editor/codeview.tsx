76681a1ff1582e7915351cb74ac185d71688d694
Joseph Emmatty Johny <josinho.seven@gmail.com>
2020-06-26 14:46:48 +0530

    corrected delete icon in editor

--- src/components/editor/codeview.tsx
+++ src/components/editor/codeview.tsx
@@ -39,8 +39,8 @@ const CodeView: React.FunctionComponent<Props> = (props) => {
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
           fill="white"
-          width="18px"
-          height="18px"
+          width="16px"
+          height="16px"
         >
           <path d="M0 0h24v24H0V0z" fill="none" />
           <path d="M3 19h18v2H3zM3 7h12v2H3zm0-4h18v2H3zm0 12h12v2H3zm0-4h18v2H3z" />
@@ -53,15 +53,15 @@ const CodeView: React.FunctionComponent<Props> = (props) => {
       <button type="button" className="codeview-controls" title="Clear code">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          height="24"
           viewBox="0 0 24 24"
-          width="24"
+          fill="white"
+          width="20px"
+          height="20px"
         >
           <path d="M0 0h24v24H0V0z" fill="none" />
-          <path d="M8 9h8v10H8z" opacity=".3" />
           <path
-            d="M15.5 4l-1-1h-5l-1 1H5v2h14V4zM6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9z"
-            fill="white"
+            d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9zm7.5-5l-1-1h-5l-1 1H5v2h14V4h-3.5z"
+            fill="red"
           />
         </svg>
         Clear
